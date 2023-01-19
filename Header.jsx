007911f19ab59d5b9f2b06b074d138a3b38e2af7
Minhajtahirk <105334859+Minhajtahirk@users.noreply.github.com>
2023-01-19 13:18:54 +0500

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,179 @@
+import React, { useRef } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.css";
+
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+const styles = {
+  
+  
+  
+    
+    
+      backgroundColor: "darken($LIGHT_C,25%)",
+      color:"red"
+    
+};
+const Header = () => {
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
+  const handleClick = event => {
+    event.currentTarget.disabled = true;
+    console.log('button clicked');
+  };
+
+  return (
+    <header className="header">
+      {/* ============ header top ============ */}
+      <div className="header__top">
+        
+          {/* <Row> */}
+            {/* <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Need Help?</span>
+                <span className="header__top__help">
+                  <i class="ri-phone-fill"></i> +1-202-555-0149
+                </span>
+              </div>
+            </Col> */}
+
+
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                {/* <Link 
+                  to="/login" 
+                  className=" d-flex align-items-center gap-1" 
+                  >
+                  <i class="ri-login-circle-line"></i>Login 
+                </Link> */}
+
+                <Link to="/login" className=" d-flex align-items-center gap-1">
+                  <i class="ri-user-line"></i> Register A car
+                </Link>
+              </div>
+            
+          
+        
+      </div>
+
+      {/* =============== header middle =========== */}
+      <div className="header__middle">
+        <Container>
+          <Row>
+            <Col lg="4" md="3" sm="4">
+              <div className="logo">
+                <h1>
+                  <Link to="/home" className=" d-flex align-items-center gap-2">
+                    <i class="ri-car-line"></i>
+                    <span>
+                      Car <br /> Dealership
+                    </span>
+                  </Link>
+                </h1>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-earth-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Pakistan</h4>
+                  <h6>Karachi, Sindh</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg="3" md="3" sm="4">
+              <div className="header__location d-flex align-items-center gap-2">
+                <span>
+                  <i class="ri-time-line"></i>
+                </span>
+                <div className="header__location-content">
+                  <h4>Sunday to Friday</h4>
+                  <h6>10am - 7pm</h6>
+                </div>
+              </div>
+            </Col>
+
+            <Col
+              lg="2"
+              md="3"
+              sm="0"
+              className=" d-flex align-items-center justify-content-end "
+            >
+              <button className="header__btn btn ">
+                <Link to="/contact">
+                  <i class="ri-phone-line"></i> Request a call
+                </Link>
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+
+      {/* ========== main navigation =========== */}
+
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i class="ri-menu-line" onClick={toggleMenu}></i>
+            </span>
+
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+              <div className="menu">
+                {navLinks.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav__active nav__item" : "nav__item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="nav__right">
+              <div className="search__box">
+                <input type="text"/>
+                {/* <span>
+                  <i class="ri-search-line"></i>
+                </span> */}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
